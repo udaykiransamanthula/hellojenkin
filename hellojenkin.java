32a1cb0cc656256a199c87d4ac65003eb529eb59
Ubuntu <ubuntu@ip-10-0-1-92.ap-south-1.compute.internal>
2020-04-16 07:03:55 +0000

    hellojenkin program

---
+++ hellojenkin.java
@@ -0,0 +1,11 @@
+class hellojenkin 
+{ 
+    // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
+    public static void main(String args[]) 
+    { 
+int i;
+for(i=0;i<=10;i++)
+        System.out.println("Hello welcome to jenkin"); 
+    } 
+} 
